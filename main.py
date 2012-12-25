a603ba7ac1b45f6d92a1d7bc2d7da91bcf8cf976
Foxboron <mcfoxax@gmail.com>
2012-12-25 23:56:48 +0100

    Added some base classes

---
+++ main.py
@@ -0,0 +1,20 @@
+import sublime
+import sublime_plugin
+
+
+class XML2CSSCommand(sublime_plugin.TextCommand):
+    """Takes a XML buffer and outputs a CSS buffer"""
+    
+    def run(self):
+        pass
+
+
+
+class CSS2XMLCommand(sublime_plugin.TextCommand):
+    """Takes a CSS buffer and outputs a XML buffer"""
+    
+    def run(self):
+        pass
+        
+
+        
